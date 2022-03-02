205d7a18142db9d34e6278ab52e6dc605994ada5
NIkita Naumov <naumovn808@gmail.com>
2022-03-02 22:24:02 +0500

    Фильтрация по диапазону

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,10 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  const i = 0;
+  const result = [];
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] >= a && arr[i] <= b) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
