d54fbeb55a736627f5d2cc3438f110e1f73f80b1
NIkita Naumov <naumovn808@gmail.com>
2022-03-04 22:31:25 +0500

    2 и 3 модуль

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,6 +1,6 @@
 function filterRange(arr, a, b) {
-  const i = 0;
-  const result = [];
+  let i = 0;
+  let result = [];
   for (i = 0; i < arr.length; i++) {
     if (arr[i] >= a && arr[i] <= b) {
       result.push(arr[i]);
