acfcf6ac5e4ee2ffd94d12d676c025478637d6a3
NIkita Naumov <naumovn808@gmail.com>
2022-03-06 15:17:32 +0500

    закончил третий модуль

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+
+  let numbersOnly = str
+    .split(',').join(' ').split(' ')
+    .filter(item => Number(item));
+  let result = {
+    min: Math.min(...numbersOnly),
+    max: Math.max(...numbersOnly),
+  };
+  return result;
 }
