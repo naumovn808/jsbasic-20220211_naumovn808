84c6312bfa70d974eaf96fe6ec62682724d713f1
NIkita Naumov <naumovn808@gmail.com>
2022-03-08 17:42:45 +0500

    1 задача 4 модуль

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const list = document.createElement('ul');
+  for (let name of friends) {
+    list.innerHTML += `<li>${name.firstName} ${name.lastName}</li>`;
+  }
+  return list;
 }
